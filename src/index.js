4dbed2cecc542a0b26887de80886fcb728b85e2c
Volkova Valeria <vvolk.valeria@gmail.com>
2023-08-08 23:56:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ root.render(
   <React.StrictMode>
     <Provider  store={store}>
     <BrowserRouter 
-     basename="/Event-Planner/
+    basename="Event-Planner"
     >
     <App />
     </BrowserRouter>
