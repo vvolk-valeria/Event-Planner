47b47116719ad395d696839087d2e0e858f852ef
Valeriia Volkova <102325355+vvolk-valeria@users.noreply.github.com>
2023-08-08 23:20:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ root.render(
   <React.StrictMode>
     <Provider  store={store}>
     <BrowserRouter 
-    // basename="/goit-react-hw-08-phonebook/"
+     basename="/Event-Planner/
     >
     <App />
     </BrowserRouter>
